4d1dfe9f29287532eabddf80958edb4021fe4cf9
e33io <info@e33.io>
2026-03-25 20:30:19 -0500

    Update comments and formatting

--- scripts/extra-repo.sh
+++ scripts/extra-repo.sh
@@ -7,25 +7,18 @@
 # Use this script at your own risk, it will overwrite existing files!
 # =============================================================================
 
-if grep -q "^\[extra\]" /etc/pacman.conf; then
-    echo "Arch [extra] repo already present in pacman.conf -- skipping."
-else
+if ! grep -q "^\[extra\]" /etc/pacman.conf; then
     echo "========================================================================"
     echo "Add Arch [extra] repo"
     echo "========================================================================"
 
-    # Install artix-archlinux-support
     sudo pacman -S --noconfirm --needed artix-archlinux-support
-    # Append after the last Artix repo block
     sudo tee -a /etc/pacman.conf > /dev/null <<'EOF'
 
-# Arch Linux repos must remain AFTER Artix repos so Artix takes precedence
+# Arch Linux repos must remain AFTER Artix repos
 [extra]
 Include = /etc/pacman.d/mirrorlist-arch
 EOF
-    # Populate Arch Linux keyring
     sudo pacman-key --populate archlinux
-    # Sync package databases
     sudo pacman -Sy
-    echo "Arch [extra] repo is now enabled."
 fi
