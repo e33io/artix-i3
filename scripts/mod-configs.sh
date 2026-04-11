051cf6c823c03d2e11787a258616e011073e98e2
e33io <info@e33.io>
2026-04-10 20:15:17 -0500

    Revert changes to scripts

--- scripts/mod-configs.sh
+++ scripts/mod-configs.sh
@@ -7,93 +7,32 @@
 # Use this script at your own risk, it will overwrite existing files!
 # =============================================================================
 
-# Update i3 startup.conf file
-awk '
-# Insert pipewire block before XDG autostart
-/^# Start XDG autostart/ {
-    print "# Start pipewire and wireplumber"
-    print "$exec pipewire"
-    print "$exec wireplumber"
-    print "$exec pipewire-pulse"
-    print ""
-    print
-    next
-}
-# Replace xss-lock comment
-/^# Start xss-lock/ {
-    print "# Start xautolock with i3lock as locker"
-    next
-}
-# Replace xss-lock command
-/^\$exec xss-lock/ {
-    print "$exec xautolock -time 5 -locker \"i3lock -i ~/.cache/i3lock/lock.png\""
-    print ""
-    next
-}
-# Add DPMS comment
-/^\$exec sleep 1 && xset dpms/ {
-    print "# Set monitor DPMS timeout"
-    print
-    next
-}
-{
-    print
-}
-' ~/dots/home/.config/i3/startup.conf > ~/.config/i3/startup.conf
+# Remove unneeded commands
+sed -i '/xss-lock/d' ~/.config/i3/startup.conf
+sed -i '/xssproxy/d' ~/.profile
 
-# Update rofi-power.sh file
-awk '
-# Remove "Logout" from options array
-/^[[:space:]]*"Logout"/ {
-    next
-}
-# Rewrite entire case block
-/^case[[:space:]]+\$choice[[:space:]]+in/ {
-    print "case $choice in"
-    print "    \"Lock\")"
-    print "        i3lock -i ~/.cache/i3lock/lock.png"
-    print "    ;;"
-    print "    \"Reboot\")"
-    print "        loginctl reboot"
-    print "    ;;"
-    print "    \"Shutdown\")"
-    print "        loginctl poweroff"
-    print "    ;;"
-    print "    *)"
-    print "        exit 0"
-    print "    ;;"
-    print "esac"
+# Add startup commands for xautolock
+printf "%s\n" "" "# Start xautolock with i3lock as locker" \
+"\$exec xautolock -time 5 -locker \"i3lock -i ~/.cache/i3lock/lock.png\"" \
+| tee -a ~/.config/i3/startup.conf > /dev/null
 
-    in_case=1
-    next
-}
-# Skip original case block entirely
-in_case {
-    if (/^esac/) {
-        in_case=0
-    }
-    next
-}
-{
-    print
-}
-' ~/dots/home/.local/bin/rofi-power.sh > ~/.local/bin/rofi-power.sh
+# Add startup commands for pipewire
+printf "%s\n" "" "# Start pipewire and wireplumber" \
+"exec --no-startup-id pipewire" "exec --no-startup-id wireplumber" \
+"exec --no-startup-id pipewire-pulse" \
+| tee -a ~/.config/i3/startup.conf > /dev/null
 
 # Add aliases for reboot and power off
 printf "%s\n" "" "# Reboot and power off" "alias reboot='loginctl reboot'" \
 "alias poweroff='loginctl poweroff'" | tee -a ~/.bashrc > /dev/null
 
-# Set default mute and default volume level
-mkdir -p ~/.config/autostart
-printf "%s\n" "[Desktop Entry]" "Version=1.0" "Type=Application" \
-"Name=audio-default" "Comment=set default mute and default volume level" \
-"Exec=sh -c 'sleep 1; pactl set-sink-mute @DEFAULT_SINK@ false; sleep 6; pactl set-sink-volume @DEFAULT_SINK@ 20%'" \
-"Icon=xfce4-mixer" "StartupNotify=false" "Terminal=false" "NoDisplay=true" \
-"Hidden=false" > ~/.config/autostart/audio-default.desktop
+# Replace systemctl with loginctl
+sed -i 's/systemctl/loginctl/g' ~/.local/bin/rofi-power.sh
+
+# Replace loginctl lock-session with i3lock
+sed -i 's/loginctl lock-session/i3lock -i ~\/\.cache\/i3lock\/lock\.png/' \
+~/.config/i3/config ~/.local/bin/rofi-power.sh
 
 # Update ranger preview_images_method
 sed -i -e '/preview_images_method ueberzug/d' \
 -e 's/#set preview_images/set preview_images/' ~/.config/ranger/rc.conf
-
-# Remove unneeded commands
-sed -i '/xssproxy/d' ~/.profile
