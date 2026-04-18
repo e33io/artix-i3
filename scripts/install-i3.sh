5177943ee5fa1662fbb911bb3f200c0c9b289951
e33io <info@e33.io>
2026-04-17 21:33:13 -0500

    Update packages

--- scripts/install-i3.sh
+++ scripts/install-i3.sh
@@ -45,15 +45,15 @@ echo "========================================================================"
 sudo pacman -S --noconfirm --needed xorg-server xorg-apps i3-wm i3status i3lock \
 xautolock polybar dmenu rofi dunst base-devel upower udisks2 gvfs nfs-utils cifs-utils \
 pipewire pipewire-openrc pipewire-pulse pipewire-pulse-openrc wireplumber wireplumber-openrc \
-libmad gst-libav rsync cronie cronie-openrc git curl wget tar less 7zip xsel xclip xbindkeys \
-xdotool yt-dlp playerctl dex xdg-desktop-portal-gtk xdg-user-dirs mate-polkit lightdm \
-lightdm-openrc lightdm-gtk-greeter lightdm-gtk-greeter-settings feh lxappearance \
-gnome-themes-extra qt5ct qt6ct ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-emoji \
+pipewire-alsa pipewire-jack libmad gst-libav rsync cronie cronie-openrc git curl wget tar \
+less 7zip xsel xclip xbindkeys xdotool yt-dlp playerctl dex xdg-desktop-portal-gtk xdg-user-dirs \
+mate-polkit lightdm lightdm-openrc lightdm-gtk-greeter lightdm-gtk-greeter-settings feh \
+lxappearance gnome-themes-extra qt5ct qt6ct ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-emoji \
 papirus-icon-theme breeze-icons plymouth pavucontrol-qt kitty python-pillowfight xterm \
-bash-completion vim nano micro fzf lazygit htop fastfetch cmus cava ranger ueberzug \
-highlight atool w3m mediainfo perl-image-exiftool thunar thunar-archive-plugin thunar-volman \
-tumbler ffmpegthumbnailer xarchiver atril scrot imagemagick imv mpv parole mousepad \
-galculator timeshift signal-desktop gpick darktable gimp inkscape filezilla libreoffice
+bash-completion vim nano micro fzf lazygit htop fastfetch cmus cava ranger ueberzug highlight \
+atool w3m mediainfo perl-image-exiftool thunar thunar-archive-plugin thunar-volman tumbler \
+ffmpegthumbnailer xarchiver atril scrot imagemagick imv mpv parole mousepad galculator \
+timeshift signal-desktop gpick darktable gimp inkscape filezilla libreoffice
 
 echo "========================================================================"
 echo "Clone custom scripts"
