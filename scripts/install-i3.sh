3a6d0ae77046e471f4dd041614ec5160187c6a93
e33io <info@e33.io>
2026-04-17 21:19:20 -0500

    Update packages and services

--- scripts/install-i3.sh
+++ scripts/install-i3.sh
@@ -42,18 +42,22 @@ echo "========================================================================"
 echo "Install i3 and other packages"
 echo "========================================================================"
 
-sudo pacman -S --noconfirm --needed xorg-server xorg-apps i3-wm i3status i3lock \
-xautolock polybar dmenu rofi dunst base-devel upower udisks2 gvfs nfs-utils cifs-utils \
-pipewire pipewire-openrc pipewire-pulse pipewire-pulse-openrc wireplumber wireplumber-openrc \
-libmad gst-libav rsync cronie cronie-openrc git curl wget tar less 7zip xsel xclip xbindkeys \
-xdotool yt-dlp playerctl dex xdg-desktop-portal-gtk xdg-user-dirs mate-polkit lightdm \
-lightdm-openrc lightdm-gtk-greeter lightdm-gtk-greeter-settings feh lxappearance \
-gnome-themes-extra qt5ct qt6ct ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-emoji \
-papirus-icon-theme breeze-icons plymouth pavucontrol-qt kitty python-pillowfight xterm \
-bash-completion vim nano micro fzf lazygit htop fastfetch cmus cava ranger ueberzug \
-highlight atool w3m mediainfo perl-image-exiftool thunar thunar-archive-plugin thunar-volman \
-tumbler ffmpegthumbnailer xarchiver atril scrot imagemagick imv mpv parole mousepad \
-galculator timeshift signal-desktop gpick darktable gimp inkscape filezilla libreoffice
+sudo pacman -S --noconfirm --needed xorg-server xorg-apps i3-wm i3status \
+i3lock xautolock polybar dmenu rofi dunst base-devel upower udisks2 gvfs \
+nfs-utils cifs-utils pipewire pipewire-openrc pipewire-pulse \
+pipewire-pulse-openrc wireplumber wireplumber-openrc dbus dbus-openrc \
+pipewire-alsa pipewire-jack libmad gst-libav rsync cronie cronie-openrc git \
+curl wget tar less 7zip xsel xclip xbindkeys xdotool yt-dlp playerctl dex \
+xdg-desktop-portal-gtk xdg-user-dirs mate-polkit lightdm lightdm-openrc \
+lightdm-gtk-greeter lightdm-gtk-greeter-settings feh lxappearance \
+gnome-themes-extra qt5ct qt6ct ttf-dejavu noto-fonts noto-fonts-cjk \
+noto-fonts-emoji papirus-icon-theme breeze-icons plymouth pavucontrol-qt \
+kitty python-pillowfight xterm bash-completion vim nano micro fzf lazygit \
+htop fastfetch cmus cava ranger ueberzug highlight atool w3m mediainfo \
+perl-image-exiftool thunar thunar-archive-plugin thunar-volman tumbler \
+ffmpegthumbnailer xarchiver atril scrot imagemagick imv mpv parole \
+mousepad galculator timeshift signal-desktop gpick darktable gimp inkscape \
+filezilla libreoffice
 
 echo "========================================================================"
 echo "Clone custom scripts"
@@ -132,9 +136,10 @@ echo "i3 installed via e33io script: $(date '+%B %d, %Y, %H:%M')" \
 | tee -a ~/.install-info > /dev/null
 sudo rc-update add lightdm default
 sudo rc-update add cronie default
-rc-update add -U pipewire default
-rc-update add -U pipewire-pulse default
-rc-update add -U wireplumber default
+sudo rc-update add dbus default
+sudo rc-update add pipewire default
+sudo rc-update add pipewire-pulse default
+sudo rc-update add wireplumber default
 rm -rf ~/dots ~/scripts
 
 echo "========================================================================"
